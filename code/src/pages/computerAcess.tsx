1d4b2997988e910034967d5957cf50cab6e93953
William Chan <willchan@bu.edu>
2023-11-20 16:10:33 -0500

    computerAccess page created

---
+++ code/src/pages/computerAcess.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CommunicationPage = () => {
+    return (
+      <div>
+        <h1>Communication</h1>
+        {/* Content for the communication page */}
+      </div>
+    );
+  };
+  
+  export default CommunicationPage;
